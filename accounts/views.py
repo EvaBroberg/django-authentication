93082c0670b925961bfed8d53adcfcab8877aee1
ieva broberg <ievabroberg@gmail.com>
2020-02-27 09:49:27 +0000

    added login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 def index(request):
     """Return the index.html file"""
@@ -10,4 +11,9 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You have successfully been logged out')
     return redirect(reverse('index'))
+
+def login(request):
+    """Return a login page"""
+    login_form = UserLoginForm()
+    return render(request, 'login.html',{'login_form': login_form})
     
