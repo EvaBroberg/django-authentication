78e8206d2c0213bbac3591c7727e48ef77181525
ieva broberg <ievabroberg@gmail.com>
2020-02-27 22:21:50 +0000

    added password reset option

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
@@ -37,5 +38,29 @@ def login(request):
 
 def registration(request):
     """Render the registration form"""
-    registration_form = UserRegistrationForm()
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    
+    if request.method ==  'POST':
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username = request.POST['username'], password = request.POST['password1'])
+
+            if user:
+                auth.login(user = user, request = request)
+                messages.success(request, 'You have been successfully registered')
+                return redirect(reverse('index'))
+            else:
+                messages.add_error(request, 'unable to register your account at this time')
+    else:
+        registration_form = UserRegistrationForm()
+         
     return render(request, 'registration.html', {'registration_form': registration_form})
+
+def user_profile(request):
+    """The users profile page"""
+    user = User.objects.get(email = request.user.email)
+    return render(request, 'profile.html', {'profile': user})
